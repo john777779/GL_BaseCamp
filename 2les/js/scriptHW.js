1fb9ab3cdb6050031d70123f1f37b2c55f0a60da
Ievgen Shmulia <john77777@ukr.net>
2016-07-11 22:53:57 +0300

    11.07

---
+++ 2les/js/scriptHW.js
@@ -0,0 +1,36 @@
+function extractCharacters(str){
+    str.toLowerCase();
+    var arr = str.split('');
+    var obj = {};
+
+    for (var i = 0; i < arr.length; i++) {
+      var str = arr[i];
+      obj[str] = true;
+    }
+    console.log(Object.keys(obj));
+};
+
+extractCharacters('abcd');
+    //['a', 'b', 'c', 'd']
+
+extractCharacters('aaaabc');
+    //['a', 'b', 'c']
+extractCharacters('Hello, world');
+    //[ 'h', 'e', 'l', 'o', ',', ' ', 'w', 'r', 'd' ];
+
+
+
+//----------myLogger----------
+
+function createLogger(prefix){
+  var now = new Date();
+  console.log(now.toISOString() + prefix);
+}
+
+var myLogger = function (some) {
+  var prefix = ' My Logger' + ': ' + some;
+  createLogger(prefix);
+};
+
+myLogger('some data');
+// 2016-06-06T09:55:44.162Z My Logger: some data
